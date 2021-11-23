c293101840348f50ee0d5a027d4972b6132f9bb8
David-14-pixel <osneider.betancur20@gmail.com>
2021-11-23 07:39:56 -0500

    eso hptaaaa

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,3 +1,7 @@
 class Film < ActiveRecord::Base
     self.table_name = "film"
+
+     belongs_to :language, foreign_key: "language_id"
+     has_many :film_categories, foreign_key: "film_id"
+     has_many :categories, through: :film_categories
 end
